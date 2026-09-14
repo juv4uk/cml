05d065c816ddef0b49395ddebbb6fe257c48b6ef
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-09-14 13:56:16 +0300

    test(x86): point vertical witness at Lisp assembler lowering

--- tests/vertical_slice_witness_test.rs
+++ tests/vertical_slice_witness_test.rs
@@ -11,10 +11,11 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use cml::elf64::Elf64Executable;
 use cml::ir::{Ir, PrimOp};
-use cml::lower;
-use cml::machine_inst::{
-    VerticalSliceError, assemble_program, items_to_gnu_asm, select_arithmetic_slice,
+use cml::lisp_asm_vertical::{
+    VerticalSliceError, items_to_gnu_asm, select_arithmetic_slice,
 };
+use cml::lower;
+use cml::machine_inst::assemble_program;
 use cml::macros::MacroExpander;
 use cml::parser;
 
