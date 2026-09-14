e0b739a89703588ff3925d284b9af0e98d1fc00a
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-09-14 15:08:40 +0300

    fix(asm): author machine macros in canonical cons core

--- contracts/machine-macro-atoms.lisp
+++ contracts/machine-macro-atoms.lisp
@@ -3,24 +3,49 @@
 ;; Lisp-authored target mechanism. These macros expand only to structured
 ;; `(x86 ...)` machine forms; they own no source-language semantic IDs and no
 ;; EN/UK/UKR/SA surface spellings. CML validates and encodes the target facts.
+;;
+;; Keep this layer inside the existing canonical macro meta-evaluator:
+;; quote/cons/car/cdr/atom/eq/cond. Do not grow host-side macro semantics just
+;; to make machine forms more convenient to author.
 
 (defmacro tag-fixnum (reg)
-  (list (quote x86) (quote shl-imm) reg 3))
+  (cons (quote x86)
+    (cons (quote shl-imm)
+      (cons reg
+        (cons 3 NIL)))))
 
 (defmacro untag-fixnum (reg)
-  (list (quote x86) (quote sar-imm) reg 3))
+  (cons (quote x86)
+    (cons (quote sar-imm)
+      (cons reg
+        (cons 3 NIL)))))
 
 (defmacro mov-imm (reg imm)
-  (list (quote x86) (quote mov-imm64) reg imm))
+  (cons (quote x86)
+    (cons (quote mov-imm64)
+      (cons reg
+        (cons imm NIL)))))
 
 (defmacro mov-reg (dst src)
-  (list (quote x86) (quote mov-reg-reg) dst src))
+  (cons (quote x86)
+    (cons (quote mov-reg-reg)
+      (cons dst
+        (cons src NIL)))))
 
 (defmacro add-imm (reg imm)
-  (list (quote x86) (quote alu-imm32) (quote add) reg imm))
+  (cons (quote x86)
+    (cons (quote alu-imm32)
+      (cons (quote add)
+        (cons reg
+          (cons imm NIL))))))
 
 (defmacro sub-imm (reg imm)
-  (list (quote x86) (quote alu-imm32) (quote sub) reg imm))
+  (cons (quote x86)
+    (cons (quote alu-imm32)
+      (cons (quote sub)
+        (cons reg
+          (cons imm NIL))))))
 
 (defmacro fast-ret ()
-  (list (quote x86) (quote ret)))
+  (cons (quote x86)
+    (cons (quote ret) NIL)))
